3caa2b5ff5d3bd3b1642a89000b7c2ffda64e779
Gunnar Westerling <g.westerling@campus.tu-berlin.de>
2023-04-13 15:33:53 +0200

    compile with built-in target 'x86_64-unknown-none'

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 // Project:     rust-os
 // Author:      Gunnar Westerling
 // Date:        10.04.2023
+// License:     MIT
+// File:        main.rs
+// Description: This is the main file of the kernel.
 
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
